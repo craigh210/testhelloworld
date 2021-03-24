63c5c7c0ddf9e936f2d976edb517be1516166f1c
craigh210 <craighunt210@gmail.com>
2021-03-24 15:53:14 -0400

    Initial commit, bad xml

---
+++ src/main/java/com/example/testhelloworld/TesthelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.testhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesthelloworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testhelloworld/TesthelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesthelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
